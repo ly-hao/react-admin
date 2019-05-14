a669e67a5626348dc34bf4b967fdaa9a0d3f6ede
ly-hao <lh90402@126.com>
2019-05-14 18:53:29 +0800

    登录页面初步完善

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@
 * 根组件
 * */
 import React,{ Component } from 'react';
-import { Button, message} from 'antd';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+//引入其他组件
+import Login from './pages/login/login';
+import Admin from './pages/admin/admin';
 export default class App extends Component{
-
-  handleClick = () => {
-    message.success('成功啦...')
-  };
   render() {
-    return <Button type="primary" onClick={this.handleClick}>测试antd</Button>
+    return (
+      <BrowserRouter>
+        <Switch> {/*只匹配其中一个*/}
+          <Route path='/login' component={Login}></Route>
+          <Route path='/' component={Admin}></Route>
+        </Switch>
+      </BrowserRouter>
+    )
   }
 }
---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,8 @@
+import React,{ Component } from 'react';
+
+/*暴露模块*/
+export default class Admin extends Component{
+  render() {
+    return <div>Admin</div>
+  }
+}
